c0adf36239d1f84de71ae2af23ce63e2dfe90585
Chris Tipney <chris@tipney.net>
2021-01-15 14:26:23 +0000

    Update main.cpp

--- ELEC143-P1-2020/main.cpp
+++ ELEC143-P1-2020/main.cpp
@@ -1,8 +1,22 @@
-//#include "mbed.h"
+#include "mbed.h"
 #include "../lib/uopmsb/uop_msb_2_0_0.h"
 #include "BMP280_SPI.h"
 
 using namespace uop_msb_200;
+
+// function prototypes
+// checkTimers() checks the three timers for temperaturte, light and pressure returns 3 bit value
+// where bits are temperature (MSB), light, pressure (LSB)
+// 7 = all three
+// 6 = temperature and light
+// 5 = temperature and pressure
+// 4 = temperature
+// 3 = light and pressure
+// 2 = light
+// 1 = pressure
+// 0 = none
+int checkTimers();  
+
 
 //On board LEDs
 DigitalOut led1(LED1);
@@ -32,15 +46,51 @@ AnalogIn ldr(AN_LDR_PIN);
 //LCD Backlight - consider PwmOut for this :)
 DigitalOut backLight(LCD_BKL_PIN);
 
+// Timers
+Timer temperatureTimer ;
+
+// Time periods in microseconds
+unsigned int temperaturePeriod = (10*1000000), temperatureTimerElapsedTime = 0;
+
 int main()
 {
+    int timerCheckValue = 0; // return value from checking timers 
     //Environmental sensor;
     EnvironmentalSensor sensor;
 
     //LCD Backlight ON
     backLight = 1;
 
+    // Start timers
+    temperatureTimer.start();
+
     while (true) {
+
+        timerCheckValue = checkTimers();
+        switch(timerCheckValue)
+        {
+            case 7:     // All three timers
+                break;
+            case 6:     // temperature and light
+                break;
+            case 5:     // temperature and pressure
+                break;
+            case 4:     // temperature
+                break;
+            case 3:     // light and pressure
+                break;
+            case 2:     // light
+                break;
+            case 1:     // pressure
+                break;
+            case 0:     // none
+                break;
+            default:    // shouldn't happen so print error message
+                printf("timerCheckValue error\n");
+                break;                
+        }
+
+        
         led1 = 1;
         led2 = 1;
         led3 = 1;
@@ -61,3 +111,16 @@ int main()
     }
 }
 
+int checkTimers()
+{
+    int returnValue = 0;
+    temperatureTimerElapsedTime = temperatureTimer.elapsed_time().count();
+    if (temperatureTimerElapsedTime > temperaturePeriod)
+    {
+        printf("Temp clock = %d\n", temperatureTimerElapsedTime); 
+        temperatureTimer.reset();
+        returnValue += 4;
+    }
+
+    return returnValue;
+}
